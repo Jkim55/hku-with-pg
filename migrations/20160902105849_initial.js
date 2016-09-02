a6920abc03d4709ab63ff4083db625430f04807a
Ji Kim <jkim55@gmail.com>
2016-09-02 11:18:58 -0600

    added migrtion files

--- migrations/20160902105849_initial.js
+++ migrations/20160902105849_initial.js
@@ -2,11 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('taco_toppings', function (table){
     table.increments();
-    table.string('name');
-    table.timestamps();
+    table.integer('price_in_cents');
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('taco_toppings')
+  return knex.schema.dropTable('taco_toppings', function (table){
+    table.dropColumn('price_in_cents')
+  })
 };
